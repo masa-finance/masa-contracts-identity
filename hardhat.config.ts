af2a697b2819d9bb65151fd60f3a5986884ca239
Sebastian Gerske <13647606+H34D@users.noreply.github.com>
2022-07-18 12:13:45 +0200

    deploy to alfajores

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,27 @@ import "hardhat-deploy";
 import "hardhat-docgen";
 import "@nomiclabs/hardhat-ethers";
 import { NetworksUserConfig } from "hardhat/types";
+import fs from "fs-extra";
+import fsx from "fs-extra";
+import { parse } from "envfile";
+
+export function getSecretParam(param: string, networkName: string | undefined) {
+  const path = `.env.${networkName}.secret`;
+
+  fsx.ensureFileSync(path);
+  return parse(fs.readFileSync(path))[param];
+}
+
+export function getPrivateKey(networkName: string | undefined) {
+  const path = `.env.${networkName}.secret`;
+
+  fsx.ensureFileSync(path);
+  const privateKey = getSecretParam("DEPLOYER_PRIVATE_KEY", networkName);
+
+  return privateKey
+    ? privateKey
+    : "0x0000000000000000000000000000000000000000000000000000000000000000";
+}
 
 const networks: NetworksUserConfig = {
   hardhat: {
@@ -9,6 +30,15 @@ const networks: NetworksUserConfig = {
     allowUnlimitedContractSize: true,
     gasPrice: "auto",
     gas: 13000000,
+  },
+
+  alfajores: {
+    url: "https://alfajores-forno.celo-testnet.org", // Localhost (default: none)
+    accounts: [getPrivateKey("alfajores"), getPrivateKey("alfajores")],
+    allowUnlimitedContractSize: true,
+    gas: 20000000,
+    gasPrice: "auto",
+    blockGasLimit: 13000000,
   },
 };
 
@@ -29,7 +59,7 @@ export default {
       default: 0,
     },
     owner: {
-      default: 1,
+      default: 0,
     },
   },
   docgen: {
