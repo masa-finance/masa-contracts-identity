b3cf9fa210a8b555225d85a9051c754419bf7ac3
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-08-16 15:33:45 +0200

    add deployment for SoulBoundName

---
+++ deploy/SoulBoundName.ts
@@ -0,0 +1,35 @@
+import { getEnvParams } from "../src/utils/EnvParams";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+
+let owner: SignerWithAddress;
+
+const func: DeployFunction = async ({
+  getNamedAccounts,
+  deployments,
+  ethers,
+  network
+}) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  [, owner] = await ethers.getSigners();
+  const env = getEnvParams(network.name);
+
+  const soulBoundIdentity = await deployments.get("SoulBoundIdentity");
+
+  const soulBoundNameDeploymentResult = await deploy("SoulBoundName", {
+    from: deployer,
+    args: [env.OWNER || owner.address, soulBoundIdentity.address, ".sol"],
+    log: true
+  });
+
+  await ethers.getContractAt(
+    "SoulBoundName",
+    soulBoundNameDeploymentResult.address
+  );
+};
+
+func.tags = ["SoulBoundName"];
+func.dependencies = ["SoulBoundIdentity"];
+export default func;
