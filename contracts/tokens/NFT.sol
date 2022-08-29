05ecb08fb4e98318e64808e4de71854b77e8ff6e
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-08-29 21:30:31 +0200

    add documentation to NFT

--- contracts/tokens/NFT.sol
+++ contracts/tokens/NFT.sol
@@ -9,6 +9,13 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+/**
+ * @title NFT
+ * @author Non-fungible token
+ * @notice Non-fungible token is a token that is not fungible.
+ * @dev Implementation of https://eips.ethereum.org/EIPS/eip-721[ERC721] Non-Fungible Token Standard,
+ * that inherits from {ERC721Enumerable}, {Pausable}, {AccessControl} and {ERC721Burnable}.
+ */
 abstract contract NFT is
     ERC721,
     ERC721Enumerable,
@@ -25,6 +32,12 @@ abstract contract NFT is
 
     string private _baseTokenURI;
 
+    /// @notice Creates a new NFT
+    /// @dev Creates a new Non-fungible token
+    /// @param owner Owner of the smart contract
+    /// @param name Name of the token
+    /// @param symbol Symbol of the token
+    /// @param baseTokenURI Base URI of the token
     constructor(
         address owner,
         string memory name,
@@ -42,10 +55,14 @@ abstract contract NFT is
         return _baseTokenURI;
     }
 
+    /// @notice Pauses the operations in the smart contract
+    /// @dev Sets an emergency stop mechanism that can be triggered by an authorized account.
     function pause() public onlyRole(PAUSER_ROLE) {
         _pause();
     }
 
+    /// @notice Unpauses the operations in the smart contract
+    /// @dev Unsets an emergency stop mechanism. It can be triggered by an authorized account.
     function unpause() public onlyRole(PAUSER_ROLE) {
         _unpause();
     }
@@ -62,6 +79,12 @@ abstract contract NFT is
         return tokenId;
     }
 
+    /// @notice A distinct Uniform Resource Identifier (URI) for a given asset.
+    /// @dev Throws if `_tokenId` is not a valid NFT. URIs are defined in RFC
+    ///  3986. The URI may point to a JSON file that conforms to the "ERC721
+    ///  Metadata JSON Schema".
+    /// @param tokenId NFT to get the URI of
+    /// @return URI of the NFT
     function tokenURI(uint256 tokenId)
         public
         view
@@ -88,6 +111,11 @@ abstract contract NFT is
 
     // The following functions are overrides required by Solidity.
 
+    /// @notice Query if a contract implements an interface
+    /// @dev Interface identification is specified in ERC-165.
+    /// @param interfaceId The interface identifier, as specified in ERC-165
+    /// @return `true` if the contract implements `interfaceId` and
+    ///  `interfaceId` is not 0xffffffff, `false` otherwise
     function supportsInterface(bytes4 interfaceId)
         public
         view
