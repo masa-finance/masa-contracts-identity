06591d7a950f41e6d465bc56e388f5fdd2c13c2e
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-08-31 16:23:48 +0200

    add state variables

--- contracts/SoulFactory.sol
+++ contracts/SoulFactory.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+import "./SoulboundIdentity.sol";
+
 /// @title Soul Factory
 /// @author Masa Finance
 /// @notice Soul Factory, that can mint new Soulbound Identities and Soul Name NFTs, paying a fee
@@ -8,6 +12,14 @@ pragma solidity ^0.8.7;
 /// This minting can be done paying a fee in ETH, USDC or CORN
 contract SoulFactory {
     /* ========== STATE VARIABLES ========== */
+
+    SoulboundIdentity public soulboundIdentity;
+ 
+    uint256 public mintingPrice; // price in stable coin
+
+    address public defaultStableCoin; // USDC
+    address public utilityToken; // $CORN
+    mapping(address => bool) public paymentMethod;
 
     /* ========== INITIALIZE ========== */
 
@@ -17,10 +29,21 @@ contract SoulFactory {
     /// @param owner Owner of the smart contract
     constructor(
         address owner,
-        address soulBoundIdentity,
-        address stableToken,
-        address erc20Token
-    ) {}
+        SoulboundIdentity _soulBoundIdentity,
+        uint256 _mintingPrice,
+        address _defaultStableCoin,
+        address _utilityToken
+    ) {
+      soulboundIdentity = _soulBoundIdentity;
+
+      mintingPrice = _mintingPrice;
+      defaultStableCoin = _defaultStableCoin;
+      utilityToken = _utilityToken;
+
+      paymentMethod[address(0)] = true; // address(0) will represent the native token (ETH)
+      paymentMethod[_defaultStableCoin] = true;
+      paymentMethod[_utilityToken] = true;
+    }
 
     /* ========== RESTRICTED FUNCTIONS ========== */
 
