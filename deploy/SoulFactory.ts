80bef418c6b6a707ea8e71629e70565d4b01a8d2
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-08-31 15:22:41 +0200

    add SoulFactory deployment script

---
+++ deploy/SoulFactory.ts
@@ -0,0 +1,44 @@
+import { getEnvParams } from "../src/utils/EnvParams";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+
+let owner: SignerWithAddress;
+
+const func: DeployFunction = async ({
+  // @ts-ignore
+  getNamedAccounts,
+  // @ts-ignore
+  deployments,
+  // @ts-ignore
+  ethers,
+  network
+}) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  [, owner] = await ethers.getSigners();
+  const env = getEnvParams(network.name);
+
+  const corn = await deployments.get("CORN");
+  const soulboundIdentity = await deployments.get("SoulboundIdentity");
+
+  const SoulFactoryDeploymentResult = await deploy("SoulFactory", {
+    from: deployer,
+    args: [
+      env.OWNER || owner.address,
+      soulboundIdentity.address,
+      ethers.constants.AddressZero,
+      corn.address
+    ],
+    log: true
+  });
+
+  await ethers.getContractAt(
+    "SoulFactory",
+    SoulFactoryDeploymentResult.address
+  );
+};
+
+func.tags = ["SoulFactory"];
+func.dependencies = ["CORN", "SoulboundIdentity"];
+export default func;
