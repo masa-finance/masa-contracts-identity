701161ebbc6dcc94f74a42993611b6f5cbe449bf
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-01 10:00:15 +0200

    add comments to IUniswapRouter

--- contracts/interfaces/dex/IUniswapRouter.sol
+++ contracts/interfaces/dex/IUniswapRouter.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.7;
 
+/// @title Uniswap Router interface
+/// @author Masa Finance
+/// @notice Interface of the Uniswap Router contract
+/// @dev This interface is used to interact with the Uniswap Router contract,
+/// and gets the most important functions of the contract. It's based on
+/// https://github.com/Uniswap/v2-periphery/blob/master/contracts/interfaces/IUniswapV2Router01.sol
 interface IUniswapRouter {}
