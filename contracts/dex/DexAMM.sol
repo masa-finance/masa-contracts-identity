8d7a5f284982063821c3fed25ce159da11e16551
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-05 16:20:33 +0200

    add variables and constants to DexAMM

--- contracts/dex/DexAMM.sol
+++ contracts/dex/DexAMM.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.7;
 import "../interfaces/dex/IUniswapRouter.sol";
 
 abstract contract DexAMM {
+    address public swapRouter;
+    address public wrappedNativeToken;
+    uint256 public constant DEADLINE =
+        0xf000000000000000000000000000000000000000000000000000000000000000;
+
     /* ========== STATE VARIABLES =========================================== */
     /* ========== INITIALIZE ================================================ */
     /* ========== RESTRICTED FUNCTIONS ====================================== */
