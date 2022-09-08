03256dd3b3d2fdc987eca8cedc0110f48396682f
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-08 14:07:35 +0200

    remove unused functions in IUniswapRouter

--- contracts/interfaces/dex/IUniswapRouter.sol
+++ contracts/interfaces/dex/IUniswapRouter.sol
@@ -8,34 +8,6 @@ pragma solidity ^0.8.7;
 /// and gets the most important functions of the contract. It's based on
 /// https://github.com/Uniswap/v2-periphery/blob/master/contracts/interfaces/IUniswapV2Router01.sol
 interface IUniswapRouter {
-    function swapExactTokensForTokens(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path,
-        address to,
-        uint256 deadline
-    ) external returns (uint256[] memory amounts);
-
-    function swapExactETHForTokens(
-        uint256 amountOutMin,
-        address[] calldata path,
-        address to,
-        uint256 deadline
-    ) external payable returns (uint256[] memory amounts);
-
-    function swapExactTokensForETH(
-        uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path,
-        address to,
-        uint256 deadline
-    ) external returns (uint256[] memory amounts);
-
-    function getAmountsOut(uint256 amountIn, address[] calldata path)
-        external
-        view
-        returns (uint256[] memory amounts);
-
     function getAmountsIn(uint256 amountOut, address[] calldata path)
         external
         view
