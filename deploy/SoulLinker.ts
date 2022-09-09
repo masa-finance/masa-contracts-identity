617bbb75953278e97662ffe31c8e6bf9bf3b5ef6
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-09 13:41:22 +0200

    verify SoulLinker

--- deploy/SoulLinker.ts
+++ deploy/SoulLinker.ts
@@ -1,3 +1,4 @@
+import hre from "hardhat";
 import { getEnvParams } from "../src/utils/EnvParams";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { DeployFunction } from "hardhat-deploy/dist/types";
@@ -19,11 +20,19 @@ const func: DeployFunction = async ({
   [, owner] = await ethers.getSigners();
   const env = getEnvParams(network.name);
 
-  await deploy("SoulLinker", {
+  const soulLinkerDeploymentResult = await deploy("SoulLinker", {
     from: deployer,
     args: [env.OWNER || owner.address],
     log: true
   });
+
+  // verify contract with etherscan, if its not a local network
+  if ((await owner.getChainId()) != 31337) {
+    await hre.run("verify:verify", {
+      address: soulLinkerDeploymentResult.address,
+      constructorArguments: [env.OWNER || owner.address]
+    });
+  }
 };
 
 func.tags = ["SoulLinker"];
