13320b0abbca490c258fbf9126322158b51b9cde
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-12 19:39:20 +0200

    solve REPLACEMENT_UNDERPRICED, when needed

--- deploy/SoulLinker.ts
+++ deploy/SoulLinker.ts
@@ -17,6 +17,9 @@ const func: DeployFunction = async ({
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
+  // const currentNonce: number = await ethers.provider.getTransactionCount(deployer);
+  // to solve REPLACEMENT_UNDERPRICED, when needed
+
   [, owner] = await ethers.getSigners();
   const env = getEnvParams(network.name);
 
@@ -24,6 +27,7 @@ const func: DeployFunction = async ({
     from: deployer,
     args: [env.OWNER || owner.address],
     log: true
+    // nonce: currentNonce + 1 // to solve REPLACEMENT_UNDERPRICED, when needed
   });
 
   // verify contract with etherscan, if its not a local network
