34eb12a6074dd10f03d59838f10162550b0d0973
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-12 19:42:01 +0200

    use network.name instead of owner.getChainId()

--- deploy/SoulFactory.ts
+++ deploy/SoulFactory.ts
@@ -34,24 +34,22 @@ const func: DeployFunction = async ({
   let wrappedNativeToken: string; // weth
   let swapRouter: string;
 
-  const chainId = await owner.getChainId();
-
-  if (chainId == 1) {
+  if (network.name == 'mainnet') {
     // mainnet
     stableCoin = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     wrappedNativeToken = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
     swapRouter = SWAPROUTER_GOERLI;
-  } else if (chainId == 5) {
+  } else if (network.name == 'goerli') {
     // goerli
     stableCoin = USDC_GOERLI;
     wrappedNativeToken = WETH_GOERLI;
     swapRouter = SWAPROUTER_GOERLI;
-  } else if (chainId == 31337) {
+  } else if (network.name == 'hardhat') {
     // hardhat
     stableCoin = USDC_GOERLI;
     wrappedNativeToken = WETH_GOERLI;
     swapRouter = SWAPROUTER_GOERLI;
-  } else if (chainId == 44787) {
+  } else if (network.name == 'alfajores') {
     // alfajores
     stableCoin = "0x37f39aD164cBBf0Cc03Dd638472F3FbeC7aE426C";
     wrappedNativeToken = "0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9";
@@ -68,7 +66,7 @@ const func: DeployFunction = async ({
       "5000000", // 5 USDC, with 6 decimals
       "3000000", // 3 USDC, with 6 decimals
       "3000000", // 3 USDC, with 6 decimals
-      chainId == 31337 || chainId == 5
+      (network.name == 'hardhat' || network.name == 'goerli')
         ? CORN_GOERLI // CORN
         : corn.address,
       stableCoin,
@@ -89,9 +87,7 @@ const func: DeployFunction = async ({
         "5000000", // 5 USDC, with 6 decimals
         "3000000", // 3 USDC, with 6 decimals
         "3000000", // 3 USDC, with 6 decimals
-        chainId == 31337 || chainId == 5
-          ? CORN_GOERLI // CORN
-          : corn.address,
+        CORN_GOERLI, // CORN
         stableCoin,
         wrappedNativeToken,
         swapRouter,
