e2f1e91a0212bff21d8a30e66b87bd9390adea5e
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-19 13:14:55 +0200

    adapt ISoulName interface

--- contracts/interfaces/ISoulName.sol
+++ contracts/interfaces/ISoulName.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.7;
 interface ISoulName {
     function mint(
         address to,
-        string memory name,
-        uint256 identityId
+        string memory name
     ) external returns (uint256);
 
     function getExtension() external view returns (string memory);
@@ -17,7 +16,7 @@ interface ISoulName {
         view
         returns (string memory sbtName, uint256 identityId);
 
-    function getIdentityNames(uint256 identityId)
+    function getIdentityNames(uint256 identityId) // TODO: getIdentityNames from account?
         external
         view
         returns (string[] memory sbtNames);
