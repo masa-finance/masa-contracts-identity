0eb03a84d3f37df7dc61cb6322149c29fae6c6a1
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-27 17:29:03 +0200

    rename owner to admin in NFT SC

--- contracts/tokens/NFT.sol
+++ contracts/tokens/NFT.sol
@@ -36,19 +36,19 @@ abstract contract NFT is
 
     /// @notice Creates a new NFT
     /// @dev Creates a new Non-fungible token
-    /// @param owner Owner of the smart contract
+    /// @param admin Administrator of the smart contract
     /// @param name Name of the token
     /// @param symbol Symbol of the token
     /// @param baseTokenURI Base URI of the token
     constructor(
-        address owner,
+        address admin,
         string memory name,
         string memory symbol,
         string memory baseTokenURI
     ) ERC721(name, symbol) {
-        _grantRole(DEFAULT_ADMIN_ROLE, owner);
-        _grantRole(PAUSER_ROLE, owner);
-        _grantRole(MINTER_ROLE, owner);
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(PAUSER_ROLE, admin);
+        _grantRole(MINTER_ROLE, admin);
 
         _baseTokenURI = baseTokenURI;
     }
