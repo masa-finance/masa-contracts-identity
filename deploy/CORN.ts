db67437a2f0210cb811315fbf9934bd1d1a1d23b
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-09-27 17:32:17 +0200

    rename owner to admin in deployment scripts

--- deploy/CORN.ts
+++ deploy/CORN.ts
@@ -3,7 +3,7 @@ import { getEnvParams } from "../src/utils/EnvParams";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { DeployFunction } from "hardhat-deploy/dist/types";
 
-let owner: SignerWithAddress;
+let admin: SignerWithAddress;
 
 const func: DeployFunction = async ({
   // @ts-ignore
@@ -20,7 +20,7 @@ const func: DeployFunction = async ({
   // const currentNonce: number = await ethers.provider.getTransactionCount(deployer);
   // to solve REPLACEMENT_UNDERPRICED, when needed
 
-  [, owner] = await ethers.getSigners();
+  [, admin] = await ethers.getSigners();
   const env = getEnvParams(network.name);
 
   const cornDeploymentResult = await deploy("CORN", {
@@ -49,7 +49,7 @@ const func: DeployFunction = async ({
 
   const corn = await ethers.getContractAt("CORN", cornDeploymentResult.address);
   await corn.transfer(
-    env.OWNER || owner.address,
+    env.ADMIN || admin.address,
     ethers.utils.parseEther("1000")
   );
 };
