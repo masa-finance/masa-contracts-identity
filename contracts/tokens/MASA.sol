3bfad5bb869c31367b953998298c47ec6b979c7f
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-10-05 15:19:38 +0200

    rename CORN to MASA smart contract

--- contracts/tokens/MASA.sol
+++ contracts/tokens/MASA.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract CORN is ERC20 {
+contract MASA is ERC20 {
     constructor() ERC20("$MASA", "$MASA") {
         _mint(msg.sender, 1000000e18);
     }
