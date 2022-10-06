b851deb01068744892f5aac8338b2c817be7272c
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-10-06 15:53:00 +0200

    add expirationDate to Link struct

--- contracts/interfaces/ISoulLinker.sol
+++ contracts/interfaces/ISoulLinker.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.7;
 
 interface ISoulLinker {
     struct Link {
-        address sourceContract;
-        uint256 sourceId;
-        address targetContract;
+        // address sourceContract;
+        // uint256 sourceId;
+        // address targetContract;
         uint256 targetId;
+        uint256 expirationDate; // if 0, no expiration date
     }
 
     function hasLinks(address token, uint256 tokenId)
