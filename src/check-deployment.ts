2ba5046e9f80ff0c7d14df3d47cbd4093b00b5aa
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-10-18 12:32:27 +0200

    read deployed smart contract addresses

--- src/check-deployment.ts
+++ src/check-deployment.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 import '@nomiclabs/hardhat-ethers';
-import { ethers } from 'hardhat';
+import { deployments, ethers } from 'hardhat';
 
 /**
  * main function
@@ -9,11 +9,27 @@ async function main() {
   const [owner] = await ethers.getSigners();
   const chainId = await owner.getChainId();
 
+  const masaDeployed = await deployments.get("MASA");
+  const soulboundIdentityDeployed = await deployments.get("SoulboundIdentity");
+  const soulboundCreditReportDeployed = await deployments.get("SoulboundCreditReport");
+  const soulNameDeployed = await deployments.get("SoulName");
+  const soulStoreDeployed = await deployments.get("SoulStore");
+  const soulLinkerDeployed = await deployments.get("SoulLinker");
+
   console.log('==============================================================================');
   console.log(`Account address: ${owner.address}`);
   console.log(`ChainId: ${chainId}`);
 
   console.log('');
+
+  console.log(`MASA address:                  ${masaDeployed.address}`);
+  console.log(`SoulboundIdentity address:     ${soulboundIdentityDeployed.address}`);
+  console.log(`SoulboundCreditReport address: ${soulboundCreditReportDeployed.address}`);
+  console.log(`SoulName address:              ${soulNameDeployed.address}`);
+  console.log(`SoulStore address:             ${soulStoreDeployed.address}`);
+  console.log(`SoulLinker address:            ${soulLinkerDeployed.address}`);
+
+  console.log('==============================================================================');
 }
 
 main()
