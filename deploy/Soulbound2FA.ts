55cd9a9d3be1291361dec071a1986e9545d5a750
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-11-02 19:45:41 +0100

    create Soulbound2FA deployment script

---
+++ deploy/Soulbound2FA.ts
@@ -0,0 +1,56 @@
+import hre from "hardhat";
+import { getEnvParams } from "../src/utils/EnvParams";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { DeployFunction } from "hardhat-deploy/dist/types";
+
+let owner: SignerWithAddress;
+
+const func: DeployFunction = async ({
+  // @ts-ignore
+  getNamedAccounts,
+  // @ts-ignore
+  deployments,
+  // @ts-ignore
+  ethers,
+  network
+}) => {
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  // const currentNonce: number = await ethers.provider.getTransactionCount(deployer);
+  // to solve REPLACEMENT_UNDERPRICED, when needed
+
+  [, owner] = await ethers.getSigners();
+  const env = getEnvParams(network.name);
+  const baseUri = `${env.BASE_URI}/2fa/`;
+
+  const constructorArguments = [env.OWNER || owner.address, baseUri];
+
+  const soulbound2FADeploymentResult = await deploy("Soulbound2FA", {
+    from: deployer,
+    args: constructorArguments,
+    log: true
+    // nonce: currentNonce + 1 // to solve REPLACEMENT_UNDERPRICED, when needed
+  });
+
+  // verify contract with etherscan, if its not a local network
+  if (network.name == "mainnet" || network.name == "goerli") {
+    try {
+      await hre.run("verify:verify", {
+        address: soulbound2FADeploymentResult.address,
+        constructorArguments
+      });
+    } catch (error) {
+      if (
+        !error.message.includes("Contract source code already verified") &&
+        !error.message.includes("Reason: Already Verified")
+      ) {
+        throw error;
+      }
+    }
+  }
+};
+
+func.tags = ["Soulbound2FAReport"];
+func.dependencies = ["SoulboundIdentity"];
+export default func;
