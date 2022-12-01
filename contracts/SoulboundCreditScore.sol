7e7d4a0c59d07d057cbe89a584eecaf24bc96c27
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-12-01 09:44:53 +0100

    rename SoulboundCreditReport to SoulboundCreditScore

--- contracts/SoulboundCreditScore.sol
+++ contracts/SoulboundCreditScore.sol
@@ -3,21 +3,21 @@ pragma solidity ^0.8.7;
 
 import "./tokens/MasaSBT.sol";
 
-/// @title Soulbound Credit Report
+/// @title Soulbound Credit Score
 /// @author Masa Finance
-/// @notice Soulbound token that represents a credit report.
-/// @dev Soulbound credit report, that inherits from the SBT contract.
-contract SoulboundCreditReport is MasaSBT {
+/// @notice Soulbound token that represents a credit score.
+/// @dev Soulbound credit score, that inherits from the SBT contract.
+contract SoulboundCreditScore is MasaSBT {
     /* ========== STATE VARIABLES =========================================== */
 
     /* ========== INITIALIZE ================================================ */
 
-    /// @notice Creates a new soulbound credit report
-    /// @dev Creates a new soulbound credit report, inheriting from the SBT contract.
+    /// @notice Creates a new soulbound credit score
+    /// @dev Creates a new soulbound credit score, inheriting from the SBT contract.
     /// @param owner Owner of the smart contract
     /// @param baseTokenURI Base URI of the token
     constructor(address owner, string memory baseTokenURI)
-        MasaSBT(owner, "Masa Credit Report", "MCR", baseTokenURI)
+        MasaSBT(owner, "Masa Credit Score", "MCS", baseTokenURI)
     {}
 
     /* ========== RESTRICTED FUNCTIONS ====================================== */
