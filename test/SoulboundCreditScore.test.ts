a4bdd403f50d303757269c6c20399e6ea208dd04
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-12-06 09:30:17 +0100

    test should fail to mint with invalid signature

--- test/SoulboundCreditScore.test.ts
+++ test/SoulboundCreditScore.test.ts
@@ -240,6 +240,25 @@ describe("Soulbound Credit Score", () => {
           )
       ).to.be.revertedWith("NOT_AUTHORIZED");
     });
+
+    it("should fail to mint with invalid signature", async () => {
+      const signatureNonAuthority = await signMintCreditScore(
+        identityId1,
+        address1,
+      );
+
+      await expect(
+        soulboundCreditScore
+          .connect(address1)
+          ["mint(address,uint256,address,uint256,bytes)"](
+            ethers.constants.AddressZero,
+            identityId1,
+            authority.address,
+            signatureDate,
+            signatureNonAuthority
+          )
+      ).to.be.revertedWith("INVALID_SIGNATURE");
+    });
   });
 
   describe("burn", () => {
