4c6222d6b768e6d97d832881c26fc8a918d5051c
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-12-07 09:53:03 +0100

    refactor masa token address in deployment scripts

--- deploy/SoulboundCreditReport.ts
+++ deploy/SoulboundCreditReport.ts
@@ -36,27 +36,32 @@ const func: DeployFunction = async ({
   let swapRouter: string;
   let wrappedNativeToken: string; // weth
   let stableCoin: string; // usdc
+  let masaCoin: string; // $masa
 
   if (network.name == "mainnet") {
     // mainnet
     swapRouter = SWAPROUTER_GOERLI;
     wrappedNativeToken = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
     stableCoin = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+    masaCoin = masa.address;
   } else if (network.name == "goerli") {
     // goerli
     swapRouter = SWAPROUTER_GOERLI;
     wrappedNativeToken = WETH_GOERLI;
     stableCoin = USDC_GOERLI;
+    masaCoin = MASA_GOERLI;
   } else if (network.name == "hardhat") {
     // hardhat
     swapRouter = SWAPROUTER_GOERLI;
     wrappedNativeToken = WETH_GOERLI;
     stableCoin = USDC_GOERLI;
+    masaCoin = MASA_GOERLI;
   } else if (network.name == "alfajores") {
     // alfajores
     swapRouter = "0xE3D8bd6Aed4F159bc8000a9cD47CffDb95F96121"; // Ubeswap
     wrappedNativeToken = "0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9";
     stableCoin = "0x37f39aD164cBBf0Cc03Dd638472F3FbeC7aE426C";
+    masaCoin = masa.address;
   } else {
     throw new Error("Network not supported");
   }
@@ -70,9 +75,7 @@ const func: DeployFunction = async ({
       swapRouter,
       wrappedNativeToken,
       stableCoin,
-      network.name == "hardhat" || network.name == "goerli"
-        ? MASA_GOERLI // MASA
-        : masa.address,
+      masaCoin,
       env.RESERVE_WALLET || admin.address
     ]
   ];
