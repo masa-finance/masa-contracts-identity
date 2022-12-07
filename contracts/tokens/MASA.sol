f75052c938e6191ba3104c00dd4d1123300bac6d
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-12-07 10:02:24 +0100

    revert remove MASA.mint() function

--- contracts/tokens/MASA.sol
+++ contracts/tokens/MASA.sol
@@ -7,4 +7,8 @@ contract MASA is ERC20 {
     constructor() ERC20("$MASA", "$MASA") {
         _mint(msg.sender, 1000000e18);
     }
+
+    function mint() external {
+        _mint(msg.sender, 1000000e18);
+    }
 }
