c67011db820ec7528db65561c489bd5f54850ba3
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-12-19 13:03:00 +0100

    remove mintPrice from ILinkedSBT

--- contracts/interfaces/ILinkedSBT.sol
+++ contracts/interfaces/ILinkedSBT.sol
@@ -4,10 +4,6 @@ pragma solidity ^0.8.7;
 import "../tokens/SBT/ISBT.sol";
 
 interface ILinkedSBT is ISBT {
-    function mintPrice() external returns (uint256);
-
-    function mintPriceMASA() external returns (uint256);
-
     function addPermissionPrice() external returns (uint256);
 
     function addPermissionPriceMASA() external returns (uint256);
