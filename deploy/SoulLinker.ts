79533283bfadbfb101ef0ebdea80721d3b7e184a
Miquel A. Cabot <miquel.cabot@gmail.com>
2022-12-21 19:43:35 +0100

    add PaymentParams utility function

--- deploy/SoulLinker.ts
+++ deploy/SoulLinker.ts
@@ -2,12 +2,7 @@ import hre from "hardhat";
 import { getEnvParams, getPrivateKey } from "../src/utils/EnvParams";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { DeployFunction } from "hardhat-deploy/dist/types";
-import {
-  MASA_GOERLI,
-  SWAPROUTER_GOERLI,
-  USDC_GOERLI,
-  WETH_GOERLI
-} from "../src/constants";
+import { paymentParams } from "../src/utils/PaymentParams";
 
 let admin: SignerWithAddress;
 
@@ -31,32 +26,10 @@ const func: DeployFunction = async ({
 
   const soulboundIdentityDeployed = await deployments.get("SoulboundIdentity");
 
-  let swapRouter: string;
-  let wrappedNativeToken: string; // weth
-  let stableCoin: string; // usdc
-  let masaCoin: string; // masa
-
-  if (network.name == "mainnet") {
-    // mainnet
-    swapRouter = SWAPROUTER_GOERLI;
-    wrappedNativeToken = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-    stableCoin = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
-    masaCoin = ethers.constants.AddressZero;
-  } else if (network.name == "goerli") {
-    // goerli
-    swapRouter = SWAPROUTER_GOERLI;
-    wrappedNativeToken = WETH_GOERLI;
-    stableCoin = USDC_GOERLI;
-    masaCoin = MASA_GOERLI;
-  } else if (network.name == "hardhat") {
-    // hardhat
-    swapRouter = SWAPROUTER_GOERLI;
-    wrappedNativeToken = WETH_GOERLI;
-    stableCoin = USDC_GOERLI;
-    masaCoin = MASA_GOERLI;
-  } else {
-    throw new Error("Network not supported");
-  }
+  const { swapRouter, wrappedNativeToken, stableCoin, masaCoin } = paymentParams(
+    network.name,
+    ethers
+  );
 
   const constructorArguments = [
     env.ADMIN || admin.address,
