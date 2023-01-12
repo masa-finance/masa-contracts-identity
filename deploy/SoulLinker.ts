b0c8648c290a1935dd826a66fdf705ab36941a9b
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-01-12 14:31:40 +0100

    remove console.log() calls

--- deploy/SoulLinker.ts
+++ deploy/SoulLinker.ts
@@ -51,14 +51,11 @@ const func: DeployFunction = async ({
   // verify contract with etherscan, if its not a local network
   if (network.name == "mainnet" || network.name == "goerli") {
     try {
-      console.log(11);
       await hre.run("verify:verify", {
         address: soulLinkerDeploymentResult.address,
         constructorArguments
       });
-      console.log(22);
     } catch (error) {
-      console.log("error", error);
       if (
         !error.message.includes("Contract source code already verified") &&
         !error.message.includes("Reason: Already Verified")
