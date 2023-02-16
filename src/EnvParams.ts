3342f82e495531edaae4c1ce1af5499a8392dc1b
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-02-16 19:33:15 +0100

    add BSCSCAN_API_KEY, POLYGONSCAN_API_KEY, CELOSCAN_API_KEY to EnvParams file

--- src/EnvParams.ts
+++ src/EnvParams.ts
@@ -45,6 +45,18 @@ export function getEtherscanApiKey() {
   return process.env.ETHERSCAN_API_KEY || getSecretParam("ETHERSCAN_API_KEY");
 }
 
+export function getBscscanApiKey() {
+  return process.env.BSCSCAN_API_KEY || getSecretParam("BSCSCAN_API_KEY");
+}
+
+export function getPolygonscanApiKey() {
+  return process.env.POLYGONSCAN_API_KEY || getSecretParam("POLYGONSCAN_API_KEY");
+}
+
+export function getCeloscanApiKey() {
+  return process.env.CELOSCAN_API_KEY || getSecretParam("CELOSCAN_API_KEY");
+}
+
 export function getCoinMarketCapApiKey() {
   return (
     process.env.COINMARKETCAP_API_KEY || getSecretParam("COINMARKETCAP_API_KEY")
