98340c4813abd497effa47bc425e7a92bf55e1ca
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-02-17 15:54:46 +0100

    fix error in hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,7 @@ import {
   getPrivateKey,
   getInfuraApiKey,
   getCoinMarketCapApiKey,
-  getEtherscanApiKey
+  getEtherscanApiKey,
   getBscscanApiKey,
   getPolygonscanApiKey
 } from "./src/EnvParams";
