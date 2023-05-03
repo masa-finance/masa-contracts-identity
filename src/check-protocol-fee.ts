024076eaf77c4c4988af4e20b3ed373f5a50b12a
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-05-03 18:18:53 +0200

    show protocol fees when price is zero

--- src/check-protocol-fee.ts
+++ src/check-protocol-fee.ts
@@ -22,15 +22,15 @@ async function showPrice(length, paymentMethod) {
     YEAR
   );
   const paymentMethodStr =
-    paymentMethod === ethers.constants.AddressZero ? "ETH" : "USDC";
+    paymentMethod === ethers.constants.AddressZero ? "ETH " : "USDC";
   console.log(
-    `price      , length ${length}, ${paymentMethodStr}: ${price.toString()}`
+    `price      , length ${length}, ${paymentMethodStr}: ${price.toString().padStart(13, ' ')}`
   );
   console.log(
-    `protocolFee, length ${length}, ${paymentMethodStr}: ${protocolFee.toString()}`
+    `protocolFee, length ${length}, ${paymentMethodStr}: ${protocolFee.toString().padStart(13, ' ')}`
   );
   console.log(
-    `TOTAL PRICE, length ${length}, ${paymentMethodStr}: ${price.add(protocolFee).toString()}`
+    `TOTAL PRICE, length ${length}, ${paymentMethodStr}: ${price.add(protocolFee).toString().padStart(13, ' ')}`
   );
 }
 
@@ -55,16 +55,8 @@ async function main() {
   );
   console.log(`protocolFee: 0`);
 
-  await showPrice(1, ethers.constants.AddressZero);
-  await showPrice(2, ethers.constants.AddressZero);
-  await showPrice(3, ethers.constants.AddressZero);
-  await showPrice(4, ethers.constants.AddressZero);
   await showPrice(5, ethers.constants.AddressZero);
 
-  await showPrice(1, env.USDC_TOKEN);
-  await showPrice(2, env.USDC_TOKEN);
-  await showPrice(3, env.USDC_TOKEN);
-  await showPrice(4, env.USDC_TOKEN);
   await showPrice(5, env.USDC_TOKEN);
 
   console.log(
@@ -73,16 +65,8 @@ async function main() {
   console.log(`protocolFee: 10%`);
   await soulStore.connect(owner).setProtocolFeePercent(10); // 10%
 
-  await showPrice(1, ethers.constants.AddressZero);
-  await showPrice(2, ethers.constants.AddressZero);
-  await showPrice(3, ethers.constants.AddressZero);
-  await showPrice(4, ethers.constants.AddressZero);
   await showPrice(5, ethers.constants.AddressZero);
 
-  await showPrice(1, env.USDC_TOKEN);
-  await showPrice(2, env.USDC_TOKEN);
-  await showPrice(3, env.USDC_TOKEN);
-  await showPrice(4, env.USDC_TOKEN);
   await showPrice(5, env.USDC_TOKEN);
 
   console.log(
@@ -92,16 +76,58 @@ async function main() {
   await soulStore.connect(owner).setProtocolFeePercent(0); // 0%
   await soulStore.connect(owner).setProtocolFeeAmount(1_000_000); // 1 USDC
 
-  await showPrice(1, ethers.constants.AddressZero);
-  await showPrice(2, ethers.constants.AddressZero);
-  await showPrice(3, ethers.constants.AddressZero);
-  await showPrice(4, ethers.constants.AddressZero);
   await showPrice(5, ethers.constants.AddressZero);
 
-  await showPrice(1, env.USDC_TOKEN);
-  await showPrice(2, env.USDC_TOKEN);
-  await showPrice(3, env.USDC_TOKEN);
-  await showPrice(4, env.USDC_TOKEN);
+  await showPrice(5, env.USDC_TOKEN);
+
+  console.log(
+    "=============================================================================="
+  );
+  console.log(`SET ALL PRICES TO ZERO`);
+  await soulStore
+    .connect(owner)
+    .setNameRegistrationPricePerYear(0, 0); // default value
+  await soulStore
+    .connect(owner)
+    .setNameRegistrationPricePerYear(1, 0); // 1 length
+  await soulStore
+    .connect(owner)
+    .setNameRegistrationPricePerYear(2, 0); // 2 length
+  await soulStore
+    .connect(owner)
+    .setNameRegistrationPricePerYear(3, 0); // 3 length
+  await soulStore
+    .connect(owner)
+    .setNameRegistrationPricePerYear(4, 0); // 4 length
+  console.log(
+    "=============================================================================="
+  );
+  console.log(`protocolFee: 0`);
+  await soulStore.connect(owner).setProtocolFeeAmount(0);
+
+  await showPrice(5, ethers.constants.AddressZero);
+
+  await showPrice(5, env.USDC_TOKEN);
+
+  console.log(
+    "=============================================================================="
+  );
+  console.log(`protocolFee: 10%`);
+  await soulStore.connect(owner).setProtocolFeePercent(10); // 10%
+
+  await showPrice(5, ethers.constants.AddressZero);
+
+  await showPrice(5, env.USDC_TOKEN);
+
+  console.log(
+    "=============================================================================="
+  );
+  console.log(`protocolFee: 1 USDC`);
+  await soulStore.connect(owner).setProtocolFeePercent(0); // 0%
+  await soulStore.connect(owner).setProtocolFeeAmount(1_000_000); // 1 USDC
+
+  await showPrice(5, ethers.constants.AddressZero);
+
   await showPrice(5, env.USDC_TOKEN);
 }
 
