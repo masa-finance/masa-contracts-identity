c1a7a2eec6519753d70f4ba2dfae8e1e8949427e
Miquel A. Cabot <miquel.cabot@gmail.com>
2023-05-23 15:05:20 +0200

    write addresses.json file

--- src/addresses.ts
+++ src/addresses.ts
@@ -1,15 +1,21 @@
 import * as fs from "fs";
 
 const deploymentsFolder = "./deployments/";
+const addressesFile = "./addresses.json";
 
 /**
  * main function
  */
 async function main() {
+  // JSON to be written in ./addresses.json file
+  let addresses = {};
+
   fs.readdirSync(deploymentsFolder).forEach((network) => {
     // for each network
     console.log(`Network: ${network}`);
+    addresses[network] = {};
     const networkFolder = `${deploymentsFolder}${network}/`;
+
     fs.readdirSync(networkFolder)
       .filter((fn) => fn.endsWith(".json"))
       .forEach((file) => {
@@ -17,9 +23,14 @@ async function main() {
         const contract = file.slice(0, -5);
         const jsonString = fs.readFileSync(`${networkFolder}${file}`, "utf-8");
         const json = JSON.parse(jsonString);
+
         console.log(`  Address: ${json.address} Contract: ${contract}`);
+        addresses[network][contract] = json.address;
       });
   });
+
+  // Write addresses to /addresses.json file
+  fs.writeFileSync(addressesFile, JSON.stringify(addresses, null, 2));
 }
 
 main()
